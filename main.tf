254b0767d9a4d012b9ab03c12c0bd4c10fe0e7e4
andersonallconsultoria <anderson@all-consultorias.com>
2024-12-15 00:46:41 -0300

    lutando

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_instance" {
-  ami           = "ami-0ff591da048329e00"  # AMI do Amazon Linux 2 (verifique se esta AMI está disponível na sua região)
+  ami           = "ami-0e2c8caa4b6378d8c"  # AMI do Amazon Linux 2 (verifique se esta AMI está disponível na sua região)
   instance_type = "t2.micro"               # Tipo de instância (t2.micro está dentro do free tier)
   subnet_id     = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
@@ -16,7 +16,7 @@ resource "aws_instance" "app_instance" {
               sudo systemctl enable docker
 
               # Clonar o repositório do GitHub
-              git clone https://github.com/lvgalvao/dashboard_aovivo_streamlit.git /app
+              git clone https://github.com/andersonallconsultoria/Projetos-Python-Git.git /app
 
               # Construir e executar o contêiner Docker
               cd /app
